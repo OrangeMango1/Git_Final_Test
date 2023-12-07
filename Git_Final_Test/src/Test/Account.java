90675d78992ac5d41ca5748519f5b24c330f0926
OrangeMango1 <liovc17@gmail.com>
2023-12-08 02:37:30 +0900

    11

--- Git_Final_Test/src/Test/Account.java
+++ Git_Final_Test/src/Test/Account.java
@@ -1,11 +1,14 @@
 package Test;
 
-public class Account {
+public class Account extends Calender {
 	double monthlyIncome = 1000000; // 수입
     double monthlyExpense = 0.0; //지출
     
  // 일간 사용 금액
     double dailyUsage = 20000.0;
+ // 하루 예산 계산
+    double dailyBudget = (monthlyIncome - monthlyExpense) / daysRemaining;
+    
     
 
 }
