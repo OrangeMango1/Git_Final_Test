509378e8cfcaada96411a626f61d8e4d22cc0fa1
OrangeMango1 <liovc17@gmail.com>
2023-12-08 02:39:17 +0900

    12

--- Git_Final_Test/src/Test/Account.java
+++ Git_Final_Test/src/Test/Account.java
@@ -4,10 +4,12 @@ public class Account extends Calender {
 	double monthlyIncome = 1000000; // 수입
     double monthlyExpense = 0.0; //지출
     
- // 일간 사용 금액
+    // 일간 사용 금액
     double dailyUsage = 20000.0;
- // 하루 예산 계산
+    // 하루 예산 계산
     double dailyBudget = (monthlyIncome - monthlyExpense) / daysRemaining;
+    // 하루 예산 경고 기준 (90%)
+    double budgetWarningThreshold = 0.9 * dailyBudget;
     
     
 
