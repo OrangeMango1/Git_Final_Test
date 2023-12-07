491be390560c4a1f261a16be4d2e53a675977a2a
OrangeMango1 <liovc17@gmail.com>
2023-12-08 02:48:39 +0900

    20

--- Git_Final_Test/src/Test/Main.java
+++ Git_Final_Test/src/Test/Main.java
@@ -5,7 +5,13 @@ import java.util.Calendar;
 public class Main {
 	public void main(String args[]) {
 		Calender Calender = new Calender();
-		
+		Account Account = new Account();
+        
+        // 결과 출력
+        Account.isLastDayOfMonth();
+        Account.BudgetWarning();
+        Calender.showDaysRemaining();
+        Account.showDailyBudget();
 	}
 
 }
