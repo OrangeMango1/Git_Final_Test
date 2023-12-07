e7e19aa80ca8b3ceb6a408abaec8fc9e1126971d
OrangeMango1 <liovc17@gmail.com>
2023-12-08 02:21:29 +0900

    55555

--- Git_Final_Test/src/Test/Calender.java
+++ Git_Final_Test/src/Test/Calender.java
@@ -3,5 +3,10 @@ package Test;
 import java.util.Calendar;
 
 public class Calender {
+	//현재 날짜 가져오기
+		Calendar calendar = Calendar.getInstance();
+	    int currentDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+	    
+	    int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);	
 
 }
